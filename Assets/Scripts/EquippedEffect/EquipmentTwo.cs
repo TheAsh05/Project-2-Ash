268f1933dac407358b0511fa31ee855aeb692d31
TheAsh05 <rashleyr@colostate.edu>
2025-03-05 15:52:46 -0700

    equipment

---
+++ Assets/Scripts/EquippedEffect/EquipmentTwo.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CreatorKitCode;
+
+public class EquipmentTwo : EquipmentItem.EquippedEffect
+{
+     public override void Equipped(CharacterData user)
+     {
+          
+     
+     }
+     
+     public override void Removed(CharacterData user)
+     {
+     
+     }
+}
